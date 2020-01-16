634c9ec07fd9152f783a4b34eeb618915ca9a802
Dionisiaca <claudia.custodiog@gmail.com>
2020-01-15 21:14:58 -0600

    Email and Fb LogIn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import { example } from './example.js';
+import { logInFn, signUpFn } from './account.js';
+import { facebook } from './facebook.js';
 
-example();
+let userEmail = document.querySelector("#emailField");
+let userPassword = document.querySelector ("#passwordField");
+
+document.querySelector("#logIn").addEventListener("click", (e) => {
+  e.preventDefault();
+  logInFn(userEmail.value, userPassword.value);
+});
+
+document.querySelector("#signUp").addEventListener("click", (e) => {
+  e.preventDefault();
+  signUpFn(userEmail.value, userPassword.value);
+});
+
+document.querySelector("#logOut").addEventListener("click", (e) => {
+  e.preventDefault();
+  logOutFn();
+});
+
+document.querySelector("#facebook").addEventListener("click", (e) => {
+  e.preventDefault();
+  //alert("k p2");
+  facebook();
+});
